5739efe1b342a756cecefe14844385ec33b72b05
chai2010 <chaishushan@gmail.com>
2018-11-04 22:23:06 +0800

    zz

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Copyright 2018 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//
+// 《青云QingCloud云计算入门 - 基于Go语言》 开源图书 - by chai2010
+//
+//	https://github.com/chai2010/qingcloud-primer-go-book
+//
+package github_com_chai2010_qingcloud_primer_go_book
